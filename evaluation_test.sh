8759a0b1dc9d2a49bd91ec2dbc28590b31202936
Qiushi Huang <hqsiswiliam@gmail.com>
2024-03-21 15:33:26 +0800

    - update the evaluation script to match the checkpoint path

--- evaluation_test.sh
+++ evaluation_test.sh
@@ -1,15 +1,15 @@
 NGPU=1 CUDA_VISIBLE_DEVICES=0 python -m torch.distributed.launch --master_port=28888 --nproc_per_node=1 evaluate.py \
     --reader_model_type=google/t5-xl-lm-adapt \
     --text_maxlength=512 \
-    --checkpoint_dir=ckpt_for_publish/xl/eval \
-    --model_path=ckpt_for_publish/xl/ckpt/checkpoint/pth/ \
+    --checkpoint_dir=ckpt/xl/eval \
+    --model_path=ckpt/xl/ckpt/checkpoint/pth/ \
     --per_gpu_batch_size=1 \
     --eval_data="data/convai2/valid.jsonl" \
     --n_context=6 \
     --retriever_n_context=6 \
     --index_mode="flat" \
     --precision=fp32     \
-    --save_index_path=ckpt_for_publish/xl/eval \
+    --save_index_path=ckpt/xl/eval \
     --write_results \
     --passages="data/corpora/story/story.jsonl" \
     --retriever_from=persona \
